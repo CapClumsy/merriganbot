27d768fa8fb5c988a52c80ef59537b044a2c643e
Akash Shah <akash.w.shah@gmail.com>
2019-04-15 16:16:57 -0400

    Added changing presence

--- Merriganbot_master.py
+++ Merriganbot_master.py
@@ -22,5 +22,6 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(game = discord.Game(name = "Listening to your every complaint"))
 
 client.run(TOKEN)
